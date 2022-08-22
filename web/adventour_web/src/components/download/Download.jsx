717a9bcb7bb301ac5d68687bcb8c736b5f94a54b
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2022-08-22 19:51:11 -0400

    Website and Website assets

---
+++ web/adventour_web/src/components/download/Download.jsx
@@ -0,0 +1,73 @@
+import {useEffect, useState} from 'react'
+
+import iphone from '../../../../../image_assets/eric_assets/iphone-signup.png'
+import android from '../../../../../image_assets/eric_assets/android-login.png'
+import androidbadge from '../../../../../image_assets/eric_assets/google-play-badge.png'
+import iosbadge from '../../../../../image_assets/eric_assets/Download-on-the-App-Store/Download-on-the-App-Store/US/Download_on_App_Store/Black_lockup/SVG/badge.svg'
+
+import './Download.css'
+
+export default function Download() {
+
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  const [showPhone, setShowPhone] = useState(() => {
+    if (windowSize <= 1440)
+      return false;
+    else
+      return true;
+  })
+  
+
+  useEffect(() => {
+    getColSize();
+    window.addEventListener('resize', () => {
+      setWindowSize(window.innerWidth);
+      if (windowSize <= 768)
+        setShowPhone(false);
+      else
+        setShowPhone(true);
+    })
+    
+    return () => {
+      window.removeEventListener('resize', () => {
+        setWindowSize(window.innerWidth);
+      });
+    }
+  }, [windowSize]) 
+
+  const getColSize = () => {
+    if (windowSize <= 1440)
+    {
+      return 'col-12'
+    }
+    else
+    {
+      return 'col-6'
+    }
+  }
+
+
+  return (
+    <div className='container down'>
+      <div className="row">
+        <div className="col left">
+          <h1>Download the Adventour app on Android and iOS</h1>
+          <p>Available on the Apple App Store and Google Play Store</p>
+        </div>
+        <div className="col">
+          <div className="row ">
+            <div className="col center">
+              <img className='iphone ' src={iphone} alt="" />
+              <img src={iosbadge} alt="" className="ios-badge" />
+            </div>
+            <div className="col center">
+              <img className='android' src={android} alt="" />
+              <img src={androidbadge} alt="" className="android-badge" />
+            </div>
+          </div> 
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ web/adventour_web/src/components/nav/Nav.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react'
+import BigLogo from '../../../../../image_assets/Logo_Adventour_Transparent.png'
+import SmallLogo from '../../../../../image_assets/Logo_Balloon_Transparent.png'
+import "./Nav.css"
+import "../../css/Containers.css"
+import { smallScreen } from '../../Constants'
+
+export default function Nav(props) {
+  
+  const classList = ['nav-list', 'top', 'middle', 'bottom', 'button', 'button-close']
+
+  const [menuOpen, setMenuOpen] = useState(() => {
+    if (window.innerWidth <= smallScreen)
+      return false;
+    else
+      return true;
+  });
+
+  const [logo, setLogo] = useState();
+
+  // Close menu if clicked outside of
+  useEffect(() => {
+    window.addEventListener('click', (e) => {
+      for (const element of classList)
+      {
+          if (e.target.className === element || window.innerWidth >= smallScreen)
+          {
+            return;
+          }
+      }
+      setMenuOpen(false);
+    })
+  
+    return () => {
+      window.removeEventListener('click', (e) => {
+        if (e.target.className !== 'nav-list', e.target.className !== 'nav-list')
+        {
+          setMenuOpen(false);
+        }
+        
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    formatNavbar();
+    window.addEventListener('resize', formatNavbar)
+  
+    return () => {
+      window.removeEventListener('resize', formatNavbar);
+    }
+  }, [])
+  
+  
+
+  const resetAll = () => {
+    props.setHome(false);
+    props.setFeatures(false);
+    props.setDownload(false);
+    props.setMeet(false);
+    props.setTerms(false);
+    props.setPrivacy(false);
+  }
+
+  const formatNavbar = () => {
+    if (window.innerWidth <= smallScreen)
+    {
+      setLogo(SmallLogo);
+      setMenuOpen(false);
+    }
+    else
+    {
+      setLogo(BigLogo);
+      setMenuOpen(true);
+    }
+  }
+
+  const changeMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+
+  return (
+    <div id='nav' className='nav-bar'>
+      <img className='logo' src={logo} onClick={() => {resetAll(); props.setHome(true)}}></img>
+      <div className={menuOpen ? 'button-close' : 'button'} onClick={changeMenu}>
+        <div className="top"></div>
+        <div className="middle"></div>
+        <div className="bottom"></div>
+      </div>
+      <div className='download'>Download</div>
+      {menuOpen ? <ul className='nav-list'>
+        <li className={props.home ? 'nav-list-item current' : 'nav-list-item'} onClick={() => {resetAll(); props.setHome(true)}}>What is Adventour?</li>
+        <li className={props.features ? 'nav-list-item current' : 'nav-list-item'} onClick={() => {resetAll();props.setFeatures(true)}}>Features</li>
+        <li className={props.download ? 'nav-list-item current' : 'nav-list-item'} onClick={() => {resetAll();props.setDownload(true)}}>Download</li>
+        <li className={props.meet ? 'nav-list-item current' : 'nav-list-item'} onClick={() => {resetAll();props.setMeet(true)}}>Meet the Team</li>
+      </ul> : null}
+    </div>
+  )
+}
