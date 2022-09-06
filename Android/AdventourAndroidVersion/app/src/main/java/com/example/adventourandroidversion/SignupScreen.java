14b11e66be7fb23eeda6e8472d85278a09097536
Wanyea Barbel <wanyea.barbel@knights.ucf.edu>
2022-09-06 02:20:02 -0400

    Updated Signup screen with datepicker functionality

--- Android/AdventourAndroidVersion/app/src/main/java/com/example/adventourandroidversion/SignupScreen.java
+++ Android/AdventourAndroidVersion/app/src/main/java/com/example/adventourandroidversion/SignupScreen.java
@@ -2,17 +2,26 @@ package com.example.adventourandroidversion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SignupScreen extends AppCompatActivity {
 
-    String usernameEditText, emailEditText, birthdateEditText, passwordEditText;
+    final Calendar birthdateCalendar = Calendar.getInstance();
+    String username, email, birthdate, password;
     Button signupButton;
     TextView loginTextView;
+    EditText birthdateDatePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,27 @@ public class SignupScreen extends AppCompatActivity {
 
         loginTextView = (TextView) findViewById(R.id.loginTextView);
         signupButton = (Button) findViewById(R.id.signupButton);
+        birthdateDatePicker = (EditText) findViewById(R.id.birthdateEditText);
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                birthdateCalendar.set(Calendar.YEAR, year);
+                birthdateCalendar.set(Calendar.MONTH, month);
+                birthdateCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabel();
+            }
+        };
+
+        birthdateDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(v.getContext(), date,
+                        birthdateCalendar.get(Calendar.YEAR),
+                        birthdateCalendar.get(Calendar.MONTH),
+                        birthdateCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         loginTextView.setOnClickListener(new View.OnClickListener() {
            public void onClick(View v) {
@@ -31,10 +61,10 @@ public class SignupScreen extends AppCompatActivity {
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                usernameEditText = findViewById(R.id.usernameEditText).toString();
-                emailEditText = findViewById(R.id.emailEditText).toString();
-                birthdateEditText = findViewById(R.id.birthdateEditText).toString();
-                passwordEditText = findViewById(R.id.passwordEditText).toString();
+                username = findViewById(R.id.usernameEditText).toString();
+                email = findViewById(R.id.emailEditText).toString();
+                birthdate = findViewById(R.id.birthdateEditText).toString(); //FIX THIS AFTER ADD DATEPICKER FEATURE
+                password = findViewById(R.id.passwordEditText).toString();
 
                 // Create new document in Firebase with the user information.
                 // If successful, go to Home intent.
@@ -42,5 +72,9 @@ public class SignupScreen extends AppCompatActivity {
         });
     }
 
-
+    private void updateLabel(){
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        birthdateDatePicker.setText(dateFormat.format(birthdateCalendar.getTime()));
+    }
 }
