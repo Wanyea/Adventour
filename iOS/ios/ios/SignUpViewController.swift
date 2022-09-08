f8b6daa411666e0afb077fdb82f761ba5345a658
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2022-09-07 21:44:18 -0400

    Added Sign Up Error Checking
    Error checking complete for the following: nickname, email, password
    
    TODO: add error checking for birthdate and the Sign Up button, alternatively deny button press function if fields are not complete

--- iOS/ios/ios/SignUpViewController.swift
+++ iOS/ios/ios/SignUpViewController.swift
@@ -29,6 +29,13 @@ class SignUpViewController: UIViewController {
         passwordConfirm.leftViewMode = UITextField.ViewMode.always
         passwordConfirm.leftView = passwordConfirmIcon
         // Do any additional setup after loading the view.
+        signUpButton.isEnabled = false
+        password.addTarget(self, action: #selector(comparePassword(_:)), for: .editingChanged)
+        passwordConfirm.addTarget(self, action: #selector(comparePassword(_:)), for: .editingChanged)
+        email.addTarget(self, action: #selector(verifyEmail(_:)), for: .editingChanged)
+        nickname.addTarget(self, action: #selector(verifyNickname(_:)), for: .editingChanged)
+
+        
     }
     
     @IBOutlet weak var nicknameIcon: UIImageView!
@@ -36,92 +43,161 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var birthdayIcon: UIImageView!
     @IBOutlet weak var passwordIcon: UIImageView!
     @IBOutlet weak var passwordConfirmIcon: UIImageView!
+    
     @IBOutlet weak var nickname: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var birthday: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var passwordConfirm: UITextField!
+    
     @IBOutlet weak var nicknameError: UILabel!
     @IBOutlet weak var emailError: UILabel!
     @IBOutlet weak var birthdayError: UILabel!
     @IBOutlet weak var errorAlert: UILabel!
     @IBOutlet weak var passwordError: UILabel!
     @IBOutlet weak var passwordConfirmError: UILabel!
+    
+    @IBOutlet weak var birthdate: UIDatePicker!
+    
     var nicknameFlag: Bool!
     var passwordFlag: Bool!
     var passwordConfirmFlag: Bool!
     var emailFlag: Bool!
     var birthdayFlag: Bool!
     
-    @IBAction func Button(sender: AnyObject) {
-        if let text = nickname.text, text.isEmpty {
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    //    @IBAction func Button(sender: AnyObject) {
+//        if let text = nickname.text, text.isEmpty {
+//            nicknameError.isHidden = false
+//            nicknameFlag = false
+//
+//        }
+//        else{
+//            nicknameError.isHidden = true
+//            nicknameFlag = true
+//        }
+//        if let text = password.text, text.isEmpty {
+//            passwordError.isHidden = false
+//            passwordFlag = false
+//
+//        }
+//        else{
+//            passwordError.isHidden = true
+//            passwordFlag = true
+//        }
+//
+//        if let text = passwordConfirm.text, text.isEmpty {
+//            passwordConfirmError.isHidden = false
+//            passwordConfirmFlag = false
+//
+//        }
+//        else{
+//            passwordConfirmError.isHidden = true
+//            passwordConfirmFlag = true
+//        }
+//        if let text = email.text, text.isEmpty {
+//            emailError.isHidden = false
+//            emailFlag = false
+//
+//        }
+//        else{
+//            emailError.isHidden = true
+//            emailFlag = true
+//        }
+//        if let text = birthday.text, text.isEmpty {
+//            birthdayError.isHidden = false
+//            birthdayFlag = false
+//
+//        }
+//        else{
+//            birthdayError.isHidden = true
+//            birthdayFlag = true
+//        }
+//        if (nicknameFlag == true) && (passwordFlag == true) && (passwordConfirmFlag == true) && (emailFlag == true) && (birthdayFlag == true){
+//            errorAlert.isHidden = true
+//        }
+//
+//        else{
+//            errorAlert.isHidden = false
+//        }
+//    }
+
+    @IBAction func signUpTapped(_ sender: Any) {
+
+        
+        
+        
+//        print(comparePassword(password: password, confirmPassword: passwordConfirm))
+        
+
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//       
+//        let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+//        
+//        // This is to get the SceneDelegate object from your view controller
+//        // then call the change root view controller function to change to main tab bar
+//        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+    }
+    
+    @objc func verifyNickname(_ textField: UITextField) {
+        if nickname.text!.count > 25 {
             nicknameError.isHidden = false
-            nicknameFlag = false
-            
-        }
-        else{
-            nicknameError.isHidden = true
-            nicknameFlag = true
-        }
-        if let text = password.text, text.isEmpty {
-            passwordError.isHidden = false
-            passwordFlag = false
-            
-        }
-        else{
-            passwordError.isHidden = true
-            passwordFlag = true
-        }
-        
-        if let text = passwordConfirm.text, text.isEmpty {
-            passwordConfirmError.isHidden = false
-            passwordConfirmFlag = false
-            
-        }
-        else{
-            passwordConfirmError.isHidden = true
-            passwordConfirmFlag = true
-        }
-        if let text = email.text, text.isEmpty {
-            emailError.isHidden = false
-            emailFlag = false
-            
-        }
-        else{
-            emailError.isHidden = true
-            emailFlag = true
-        }
-        if let text = birthday.text, text.isEmpty {
-            birthdayError.isHidden = false
-            birthdayFlag = false
-            
-        }
-        else{
-            birthdayError.isHidden = true
-            birthdayFlag = true
-        }
-        if (nicknameFlag == true) && (passwordFlag == true) && (passwordConfirmFlag == true) && (emailFlag == true) && (birthdayFlag == true){
-            errorAlert.isHidden = true
-        }
-        
-        else{
             errorAlert.isHidden = false
+            signUpButton.isEnabled = false
+        } else {
+            nicknameError.isHidden = true
+            errorAlert.isHidden = true
+            
         }
     }
-
-    @IBAction func loginTapped(_ sender: Any) {
+    
+    @objc func verifyEmail(_ textField: UITextField) {
+//        verify that email has @ and period
+        let emailPattern = #"^\S+@\S+\.\S+$"#
         
-                // ...
-                // after login is done, maybe put this in the login web service completion block
+        var result = email.text!.range(
+            of: emailPattern,
+            options: .regularExpression
+        )
+        
+        let validEmail = (result != nil)
+        
+        if (!validEmail) {
+            emailError.isHidden = false
+            errorAlert.isHidden = false
+            signUpButton.isEnabled = false
+        } else {
+            emailError.isHidden = true
+            errorAlert.isHidden = true
+            
+        }
+        
+    }
+    
+    @objc func comparePassword(_ textField: UITextField) {
+//        verify password and confirmPassword are the same
+        if (password.text == passwordConfirm.text) {
+            passwordConfirmError.isHidden = true
+            errorAlert.isHidden = true
+            
+        }
+        else {
+            passwordConfirmError.isHidden = false
+            errorAlert.isHidden = false
+            signUpButton.isEnabled = false
+        }
+        
+        
+        
+    }
+    
+    func verifyBirthdate(bithdate: UIDatePicker) -> Bool {
+//        verify user is 13 or older
+        
+        return true;
+    }
 
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-               
-                let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
-                
-                // This is to get the SceneDelegate object from your view controller
-                // then call the change root view controller function to change to main tab bar
-                (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
-            }
     
     //override func viewWillAppear(_ animated: Bool) {
     //    super.viewWillAppear(animated)
