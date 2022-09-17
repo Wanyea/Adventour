6015da2d42f2b17abbf976fd84f7dd9b2b666fda
charley1234 <charleymoore2016@gmail.com>
2022-09-17 18:37:52 -0400

    search filter updates

--- iOS/ios/ios/StartViewController.swift
+++ iOS/ios/ios/StartViewController.swift
@@ -6,6 +6,12 @@ class StartViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let step: Float = 1
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        let roundedValue = round(sender.value / step) * step
+        sender.value = roundedValue
+    }
         
 
 
