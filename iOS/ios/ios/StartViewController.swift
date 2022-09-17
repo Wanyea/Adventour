6432739704dbc6bf0aedd2be241a17c4ffeeaf59
charley1234 <charleymoore2016@gmail.com>
2022-09-17 19:52:44 -0400

    Balloon resizing on start page and not now button border

--- iOS/ios/ios/StartViewController.swift
+++ iOS/ios/ios/StartViewController.swift
@@ -1,12 +1,24 @@
 import UIKit
 
 class StartViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var notNow: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Adding the bordercolor on the not now button. Actual border is in its runtime attributes.
+        
+        notNow?.layer.borderColor = UIColor.red.cgColor
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    
+    
+    //makes the sliders "step".
     let step: Float = 1
     @IBAction func sliderValueChanged(sender: UISlider) {
         let roundedValue = round(sender.value / step) * step
