db0edd12e080257c1524b7f7ba59045139e73971
Ryan <70408766+BossRocket@users.noreply.github.com>
2022-09-18 15:47:12 -0400

    Forced light theme
    On devices with Android dark theme, the app would try to open with dark colors instead of what we intended. These changes force light theme on those devices.

--- Android/AdventourAndroidVersion/app/src/main/java/com/adventour/android/StartAdventour.java
+++ Android/AdventourAndroidVersion/app/src/main/java/com/adventour/android/StartAdventour.java
@@ -1,6 +1,7 @@
 package com.adventour.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ public class StartAdventour extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_adventour);
 
