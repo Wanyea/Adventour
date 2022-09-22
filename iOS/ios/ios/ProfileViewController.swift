cffd5672edefb249aaf776055c75eb07650e855c
NYardich <nickyardich@gmail.com>
2022-09-21 22:33:48 -0400

    Removes Scrollviews, Cleans up ProfileViewController

--- iOS/ios/ios/ProfileViewController.swift
+++ iOS/ios/ios/ProfileViewController.swift
@@ -8,16 +8,6 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    @IBOutlet weak var AdventourStack: UIStackView!
-    @IBOutlet weak var BeaconStack: UIStackView!
-    
-    @IBOutlet weak var PassportView: UIView!
-    @IBOutlet weak var AdventourView: UIView!
-    @IBOutlet weak var BeaconBoardView: UIView!
-    
-    @IBOutlet weak var Username: UILabel!
-    @IBOutlet weak var DOB: UILabel!
-    @IBOutlet weak var ProfileDesc: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
