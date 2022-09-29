13b0cdeef431adc34a34ec63095d5ad9e68d5260
Ryan <70408766+BossRocket@users.noreply.github.com>
2022-09-28 21:22:22 -0400

    Removed many fields from passport more info
    Removed many fields from passport more info, reflecting the decisions made in our previous meetings

--- Android/AdventourAndroidVersion/app/src/main/java/com/adventour/android/PassportMoreInfo.java
+++ Android/AdventourAndroidVersion/app/src/main/java/com/adventour/android/PassportMoreInfo.java
@@ -13,32 +13,5 @@ public class PassportMoreInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passport_more_info);
-
-        // Initialize and assign variable
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.passport);
-
-        // Perform item selected listener
-        bottomNavigationView.setOnItemSelectedListener((BottomNavigationView.OnItemSelectedListener) item -> {
-
-            switch (item.getItemId()) {
-                case R.id.passport:
-                    startActivity(new Intent(getApplicationContext(), Passport.class));
-                    overridePendingTransition(0, 0);
-                    return true;
-                case R.id.start_adventour:
-                    startActivity(new Intent(getApplicationContext(), StartAdventour.class));
-                    overridePendingTransition(0, 0);
-                    return true;
-                case R.id.beacons:
-                    startActivity(new Intent(getApplicationContext(), Beacons.class));
-                    overridePendingTransition(0, 0);
-                    return true;
-            }
-            return false;
-        });
-
     }
 }
