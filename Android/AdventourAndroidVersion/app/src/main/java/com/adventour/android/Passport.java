61c47fd0b64e2d964cb3fbf4fc636f5f01a38418
Ryan <70408766+BossRocket@users.noreply.github.com>
2022-10-01 10:15:33 -0400

    Programmatic card generation (don't merge!)
    In-progress programmatic generation of cards. Pushed for the team's reference only, do not merge into Android.

--- Android/AdventourAndroidVersion/app/src/main/java/com/adventour/android/Passport.java
+++ Android/AdventourAndroidVersion/app/src/main/java/com/adventour/android/Passport.java
@@ -2,24 +2,46 @@ package com.adventour.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.common.io.Resources;
+
+import org.w3c.dom.Text;
 
 public class Passport extends AppCompatActivity {
 
     ImageButton imageButton;
     ImageButton hamburgerMenu;
 
+    Context context;
+    Button button;
+    LinearLayout linearLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passport);
+
+        context = getApplicationContext();
+        button = findViewById(R.id.addCard);
+        linearLayout = findViewById(R.id.linearLayout);
+
 
         imageButton = (ImageButton) findViewById(R.id.imageButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +56,12 @@ public class Passport extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), Settings.class);
                 startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                newPreviousAdventourCard();
             }
         });
 
@@ -60,5 +88,63 @@ public class Passport extends AppCompatActivity {
             }
             return false;
         });
+    }
+
+    public void newPreviousAdventourCard() {
+        FrameLayout.LayoutParams layoutParams;
+        FrameLayout.LayoutParams dateParams;
+        FrameLayout.LayoutParams locationParams;
+        FrameLayout.LayoutParams moreInfoParams;
+        FrameLayout frameLayout;
+        ImageView imageView;
+        TextView dates;
+        TextView locations;
+        TextView moreInfo;
+
+        imageView = new ImageView(context);
+        dates = new TextView(context);
+        locations = new TextView(context);
+        frameLayout = new FrameLayout(context);
+        moreInfo = new TextView(context);
+        layoutParams = new FrameLayout.LayoutParams (FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        dateParams = new FrameLayout.LayoutParams (FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        locationParams = new FrameLayout.LayoutParams (FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        moreInfoParams = new FrameLayout.LayoutParams (FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+
+        imageView.setPadding(0, 0, 0, 40);
+        imageView.setImageResource(R.drawable.ic_stamp_card);
+
+        dateParams.setMargins(30, 25, 0, 0);
+
+        dates.setLayoutParams(dateParams);
+        dates.setText("3/18/2000 - 3/19/2000");
+        dates.setTextSize(18);
+        dates.setTextColor(getResources().getColor(R.color.red_variant));
+        dates.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+
+        locationParams.setMargins(70, 150, 0, 0);
+
+        locations.setLayoutParams(locationParams);
+        locations.setText("\u25CF University of Central Florida\n\u25CF The Cloak and Blaster\n\u25CF American Escape Rooms Orlando");
+        locations.setTextSize(16);
+        locations.setTextColor(getResources().getColor(R.color.red_variant));
+        locations.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+
+        moreInfoParams.setMargins(30, 420, 0, 0);
+
+        moreInfo.setLayoutParams(moreInfoParams);
+        moreInfo.setText("> More...");
+        moreInfo.setTextSize(16);
+        moreInfo.setTextColor(getResources().getColor(R.color.red_variant));
+        moreInfo.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
+        moreInfo.setClickable(true);
+        moreInfo.setFocusable(true);
+
+        frameLayout.setLayoutParams(layoutParams);
+        frameLayout.addView(imageView);
+        frameLayout.addView(dates);
+        frameLayout.addView(locations);
+        frameLayout.addView(moreInfo);
+        linearLayout.addView(frameLayout);
     }
 }
