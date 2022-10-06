cbdf3d1c4dce5472f79d5323d8334e4bee69409c
charley1234 <charleymoore2016@gmail.com>
2022-10-06 17:02:46 -0400

    more beaconpost ui fix

--- iOS/ios/ios/BeaconPostViewController.swift
+++ iOS/ios/ios/BeaconPostViewController.swift
@@ -1,6 +1,21 @@
 import UIKit
 import MapKit
 
+extension UIImageView{
+    @IBInspectable var borderColor : UIColor? {
+        get{
+            if let color = layer.borderColor{
+                return UIColor(cgColor: color)
+            }
+            else{
+                return nil
+            }
+        }
+        set {layer.borderColor = newValue?.cgColor}
+}
+}
+
+
 class BeaconPostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
