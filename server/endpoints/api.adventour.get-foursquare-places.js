86678b9d651b7fed327fa23bb339d27f5d697e8e
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2022-10-12 13:42:44 -0400

    Recover Lost Server
    Man that was a close one, good thing I already deployed it.

---
+++ server/endpoints/api.adventour.get-foursquare-places.js
@@ -0,0 +1,77 @@
+
+var admin = require("firebase-admin");
+
+const router = require('express').Router()
+const foursquare = require('api')('@fsq-docs/v1.0#3qzzviakl47e7mgp');
+
+require("dotenv").config(); 
+
+module.exports = router
+const key = process.env.FOURSQUARE_API_KEY
+
+
+router.post('/get-foursquare-places', async (req, res, next) => {
+
+    const { uid, ids } = req.body
+
+    
+
+    admin.auth()
+        .getUser(uid)
+        .then(userRecord => {
+            console.log(`Successfully fetched user data: ${userRecord.toJSON()}`)
+
+            // console.log("Body")
+            // console.log("===============")
+            // console.log(req.body)
+            // console.log("===============")
+            // console.log("ids")
+            // console.log("===============")
+            // console.log(ids)
+            // console.log("===============")
+
+
+            foursquare.auth(key);
+            
+            Promise.all(ids.map(item => {
+                return foursquare.placeDetails({
+                    fields: 'fsq_id%2Cname%2Clocation%2Cdescription%2Ctel%2Cwebsite%2Crating%2Cpopularity%2Cprice%2Cphotos',
+                    fsq_id: item
+                })
+            }))
+            .then(results => {
+                // console.log("Results")
+                // console.log("===============")
+                // console.log(results)
+                // console.log("===============")
+                
+                var ret = {
+                    status: 200,
+                    error: "",
+                    results: results
+                }
+                
+                res.status(200).json(ret)
+                
+            })
+            .catch(error => {
+                console.log(error)
+                var ret = {
+                    status: 400,
+                    error: error,
+                    results: []
+                }
+                res.status(400).json(ret)
+            })
+        })
+        .catch(error => {
+            console.log("Invalid user: access denied.")
+            console.log(error)
+            var ret = {
+                status: 401,
+                error: error,
+                results: []
+            }
+            res.status(401).json(ret)
+        })
+})
--- server/server.js
+++ server/server.js
@@ -1,37 +1,46 @@
 /* Adventour Node Server */
-/* Serving up Google Maps requests since 2022 */
+/* Serving up Foursquare requests since 2022 */
 /* Author: Glenn Eric Hartwell */
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 // Dependencies
 const express = require("express")
 const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const admin = require('firebase-admin')
+var serviceAccount = require("./firebaseAdmin.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
 
 
 // Create Express application
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-var test = require("./api/test")
-var testapi = require("./api/test-api")
+var getPlaceById = require("./endpoints/api.adventour.get-place-by-id")
+var getFoursquarePlaces = require("./endpoints/api.adventour.get-foursquare-places")
+var getAdventourPlace = require("./endpoints/api.adventour.get-adventour-place")
 
-app.use("/", test)
 
-app.use("/", testapi)
+
+
+app.use(getPlaceById);
+app.use(getFoursquarePlaces);
+app.use(getAdventourPlace);
+
 
 app.get('/', (req, res) => {
-    res.send("Hello")
-})
-
-app.get("/test", (req, res) => {
-    res.status(200).json({
-        message: "This is a test",
-        error: ""
-    })
+    res.send("This is the root of the Adventour Node/Express Server<br/>Available endpoints can be found <a href=\"https://app.swaggerhub.com/apis/glerichartwell/Adventour/1.0.0#/developers/\">here</a>")
 })
 
 
+
+app.set('json spaces', 2)
 app.listen(PORT);
 console.log("listening", PORT)
