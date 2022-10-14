f22fc0d628ab5cd95b3f01ef168833fa86d079a5
NYardich <nickyardich@gmail.com>
2022-10-13 21:02:33 -0400

    Hooks up getting backend data for profile and edit profile page, fixes styling on edit profile

--- iOS/ios/ios/ProfileViewController.swift
+++ iOS/ios/ios/ProfileViewController.swift
@@ -6,24 +6,47 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewController: UIViewController {
     
+    var user: User!
+    
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var mantra: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let user = Auth.auth().currentUser {
+            self.user = user
+        }
+        getUserData()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getUserData() {
+        let db = Firestore.firestore()
+        db.collection("Adventourists").document(self.user.uid).getDocument { document, error in
+            if let document = document, document.exists {
+                document.data().map { user in
+                    print(user)
+                    if let birthday = user["birthday:"] as? String {
+                        print(birthday)
+                        self.birthday.text = birthday
+                    }
+                    if let username = user["nickname"] as? String {
+                        print(username)
+                        self.username.text = username
+                    }
+                    if let mantra = user["mantra"] as? String {
+                        print(mantra)
+                        self.mantra.text = mantra
+                    }
+                    
+                }
+            }
+        }
     }
-    */
 
 }
