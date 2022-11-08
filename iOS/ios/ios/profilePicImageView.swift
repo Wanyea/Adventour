f7cc89b67e6b99ea46f3875a09b3d3e6818b4155
charley1234 <charleymoore2016@gmail.com>
2022-11-08 14:05:59 -0500

    sign up changes

---
+++ iOS/ios/ios/profilePicImageView.swift
@@ -0,0 +1,23 @@
+//
+//  profilePicImageView.swift
+//  ios
+//
+//  Created by Cassidy Moore on 11/7/22.
+//
+
+import UIKit
+
+class profilePicImageView: UIImageView {
+    
+    var iosPfpRef: String!
+    var androidPfpRef: String!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
